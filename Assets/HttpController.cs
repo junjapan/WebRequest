861d2d66539dc398d263526488e877e03aa430d3
jun <watanabejapan2trap@gmail.com>
2022-02-19 10:20:21 +0900

    webRequest基本テキストと画像

---
+++ Assets/HttpController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HttpController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(HttpConnect());
+    }
+    IEnumerator HttpConnect()
+    {
+        string url = "https://joytas.net/php/hello.php";
+
+        //2018年から出てきたクラス。uwrにget通信が出来るインスタンスを入れる。
+        UnityWebRequest uwr = UnityWebRequest.Get(url);
+        //以下で実際に通信処理。yield returnなので return以降も処理される。
+        yield return uwr.SendWebRequest();
+        //isNetworkErrorはwifiがつながらないとか。
+        //isHttpErrorは、wifiつながったけど、404エラーとか。
+        if (uwr.isHttpError || uwr.isNetworkError)
+        {
+            Debug.Log(uwr.error);
+        }
+        else
+        {
+            Debug.Log(uwr.downloadHandler.text);
+        }
+    }
+
+}
---
+++ Assets/ImageController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class ImageController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(HttpConnect());
+    }
+
+    IEnumerator HttpConnect()
+    {
+        string url = "https://joytas.net/php/man.jpg";
+        UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(url);
+        yield return uwr.SendWebRequest();
+        if(uwr.isHttpError|| uwr.isNetworkError)
+        {
+            Debug.Log(uwr.error);
+        }
+        else
+        {
+            Texture texture = DownloadHandlerTexture.GetContent(uwr);
+
+            Sprite sp = Sprite.Create((Texture2D)texture,
+                new Rect(0,0,texture.width,texture.height),
+                new Vector2(0.5f,0.5f)
+                );
+
+            Image image = GetComponent<Image>();
+
+            image.rectTransform.sizeDelta = new Vector2(texture.width,texture.height);
+
+            image.sprite = sp;
+        }
+    }
+
+}
