71fb08f7397d64b54e453cab71be49024c49e380
jun <watanabejapan2trap@gmail.com>
2022-02-19 12:47:35 +0900

    web通信

---
+++ Assets/PostController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class PostController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(HttpConnect());
+
+    }
+
+    IEnumerator HttpConnect()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("x", 5);
+        form.AddField("y", 8);
+
+        string url= "https://joytas.net/php/calc.php";
+        UnityWebRequest uwr = UnityWebRequest.Post(url, form);
+        yield return uwr.SendWebRequest();
+        if (uwr.isError || uwr.isNetworkError)
+        {
+            Debug.Log(uwr.error);
+        }
+        else
+        {
+            Debug.Log(uwr.downloadHandler.text);
+        }
+    }
+
+}
